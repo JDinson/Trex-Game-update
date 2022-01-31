41a76d8974694a7aa23829776b28ec40d5b0e4a6
JDinson <96398344+JDinson@users.noreply.github.com>
2022-01-31 18:12:26 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,96 @@
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+
+
+
+
+var score;
+
+
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex2.png","trex3.png");
+  trex_collided = loadImage("trex_collided.png");
+  
+  groundImage = loadImage("ground2.png");
+  cloudImage =loadImage("cloud.png");
+ 
+  
+}
+
+function setup() {
+
+  createCanvas(600,200)
+  
+  //create a trex sprite
+  trex = createSprite(50,160,20,50);
+  trex.addAnimation("running", trex_running);
+  trex.scale = 0.5;
+  
+  //create a ground sprite
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  ground.velocityX = -4;
+  
+  //creating invisible ground
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  
+  //generate random numbers
+  var rand =  Math.round(random(1,100))
+  console.log(rand)
+
+}
+
+function draw() {
+  //set background color
+  background(180);
+  
+  console.log(trex.y)
+  
+  
+  
+  // jump when the space key is pressed
+  if(keyDown("space")&& trex.y >= 100) {
+    trex.velocityY = -10;
+  }
+  
+  trex.velocityY = trex.velocityY + 0.8
+  
+  if (ground.x < 0){
+    ground.x = ground.width/2;
+  }
+  
+  //stop trex from falling down
+  trex.collide(invisibleGround);
+  
+  //Spawn Clouds
+  spawnClouds()
+  
+  drawSprites();
+}
+
+//function to spawn the clouds
+// call function
+function spawnClouds(){
+ // write your code here 
+// write if condition for framecount to check if remainder is 0 
+ if (frameCount%60===0){
+   //create clouds
+  cloud=createSprite(600,100,40,10);
+  // make clouds move
+  cloud.velocityX=-3
+  // add cloud image
+  cloud.addImage (cloudImage);
+  // add cloud size
+  cloud.scale=0.6
+// make trex go in front of clouds
+  cloud.depth=trex.depth;
+  trex.depth=trex.depth+1
+  // make the clouds come in a random oder so it is more realistic
+  cloud.y=Math.round(random(10,60))
+  }
+}
+
+
+
